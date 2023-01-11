5b1881cd9f3d4c01046ceac14e1e18b8e2fc8473
danielchmuro <109912056+danielchmuro@users.noreply.github.com>
2023-01-11 11:48:13 +0100

    Update vnet.tf

--- terraform/modules/1/vnet.tf
+++ terraform/modules/1/vnet.tf
@@ -4,4 +4,5 @@ module "network" {
   address_spaces      = ["10.0.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
+  use_for_each        = false  
 }
